df8f138476b5d1533b1bd4e84c9ef6787125b180
ZhichengChen <czc555@gmail.com>
2017-12-28 01:44:16 +0000

    change server port

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ wss.broadcast = function broadcast(data) {
     });
   };
 
-server.listen(8080, function listening() {
+server.listen(process.env.PORT || 3000, function listening() {
   console.log('Listening on %d', server.address().port);
 });
